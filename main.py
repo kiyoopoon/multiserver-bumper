a109d4f92885d0b8df09d95ff0c63d40ea65b51f
Ayanokoji <87245088+kiyoopon@users.noreply.github.com>
2023-10-31 15:22:45 +0600

    Update main.py
    Fixed rate limit issue

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ CHANNEL_IDS = []
 @bot.event
 async def on_ready():
     print("Logged in!")
-
     bot.loop.create_task(auto())
 
 async def auto():
     while True:
-        await asyncio.sleep(7200)
         channels = CHANNEL_IDS
         for channel_id in channels:
             channel = bot.get_channel(channel_id)
@@ -23,13 +21,15 @@ async def auto():
                 log_channel = bot.get_channel(LOG_CHANNEL)
                 await log_channel.send(f"```Sent bump command!\nChannel ID: {channel_id}```")
                 await channel.send("bump")
-                await asyncio.sleep(1200)
+                await asyncio.sleep(1200)  # wait after each message
+        await asyncio.sleep(7200)  # wait after each round of messages
 
 @bot.command()
 async def ump(ctx):
     channel = bot.get_channel(ctx.channel.id)
     async for command in channel.slash_commands():
         if command.name == "bump":
-            await command.invoke(ctx)
+            await command(channel)
+            await asyncio.sleep(60)
 
 bot.run(TOKEN)
