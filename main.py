12dcffccfc12f5be5b30367a3f8f8ed27c208dfe
aidan <67598470+CodeByAidan@users.noreply.github.com>
2024-07-04 18:31:52 -0400

    Make `LOG_CHANNEL` optional...
    No need to force the user to have a log channel - just makes it verbose...

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint
 bot = Bot(command_prefix="b", self_bot=True, chunk_guilds_at_startup=False)
 
 TOKEN = "YOUR TOKEN"
-LOG_CHANNEL = "YOUR LOG CHANNEL" # Put the channel id without quotation marks
+LOG_CHANNEL = "YOUR LOG CHANNEL" # OPTIONAL - Put the channel id without quotation marks
 CHANNEL_IDS = []
 
 @bot.event
@@ -18,8 +18,9 @@ async def auto():
         channels = CHANNEL_IDS
         for channel_id in channels:
             if channel := bot.get_channel(channel_id):
-                log_channel = bot.get_channel(LOG_CHANNEL)
-                await log_channel.send(f"```Sent bump command!\nChannel ID: {channel_id}```")
+                if LOG_CHANNEL:
+                    log_channel = bot.get_channel(LOG_CHANNEL)
+                    await log_channel.send(f"```Sent bump command!\nChannel ID: {channel_id}```")
                 await channel.send("bump")
                 await asyncio.sleep(randint(1800, 1820))  # wait after each message
         await asyncio.sleep(randint(7200, 7220))  # wait after each round of messages
